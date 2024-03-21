5c0711105077a3540c21a3e7ba1b3c6c3d53fd68
MasterGamer2275 <MasterGamer2275@users.noreply.github.com>
2024-03-22 01:00:01 +0530

    form completed to update/delete supplier record. not working .

--- src/forms/supplier.php
+++ src/forms/supplier.php
@@ -17,8 +17,75 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
+/* The popup form - hidden by default */
+.form-popup {
+  display: none;
+  position: fixed;
+  max-width: 300px;
+  width: auto;
+  bottom: 0;
+  right: 20px;
+  border: 3px solid #f1f1f1;
+  z-index: 9;
+  color: white;
+}
 
+/* Add styles to the form container */
+.form-container {
+  max-width: 300px;
+  width: 300px;
+  padding: 10px;
+  background-color: rgb(173, 103, 79);
+ 
+}
+
+/* Full-width input fields */
+.form-container input[type=text], .form-container input[type=password], .form-container input[type=email], .form-container input[type=date]{
+  width: 90%;
+  height: 0px;
+  padding: 15px;
+  margin: 5px 0 2px 0;
+  border: none;
+  font-size: 16px;
+  background: rgb(240, 227, 210);
+}
+.form-container select{
+  width: 90%;
+  padding: auto;
+  border: none;
+  background: rgb(240, 227, 210);
+}
+
+/* When the inputs get focus, do something */
+.form-container input[type=text]:focus, .form-container input[type=password]:focus {
+  background-color: rgb(240, 227, 220);
+  outline: none;
+}
+
+/* Set a style for the submit/login button */
+.form-container .btn {
+  background: rgb(240, 227, 210);
+  color: rgb(240, 227, 210);
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  margin-bottom:10px;
+  opacity: 0.2;
+}
+
+/* Add a red background color to the cancel button */
+.form-container .cancel {
+  background-color: #CEB9A4;
+  opacity: 0.2;
+}
+
+/* Add some hover effects to buttons */
+.form-container .btn:hover, .open-button:hover {
+  opacity: 1;
+}
 select {
 width: 13%;
 /* width: 120px;*/
@@ -55,101 +122,209 @@ input(type=number) {
   -moz-appearance: textfield;
 }
 </style>
+<div id="id01">
 
+  <form action="forms_action_page.php" method="post">
+    <p>Suppliers : Please feed the new supplier name, address and GST. in to the MES System.<p>
+    <label for="Sname"><b>Supplier Name: *</label>
+    <input type= "text" id = "Sname" name = "Sname" required size="75">
+    <label for="SuGST"><b>GSTIN/UIN: *</label>
+    <input type = "text" id = "SuGST" name = "SuGST" maxlength = "15" size = "15" required>
+    <label for="SIGST"><b>(IGST):</label>
+    <input type = "checkbox" id = "SIGST" name = "SIGST"><br><br>
+    <label for="SAddr"><b>Address:</label>
+    <input type = "text" id = "SAddr" name = "SAddr" size="75" maxlength = "75">
+    <label for="SCity"><b>City:</label>
+    <input type = "text" id = "SCity" name = "SCity" size="32" maxlength = "32"><br><br>
+    <label for="SState"><b>State:</label>
+      <select name="Sstate" id="Sstate">
+        <option value="Andhra Pradesh">Andhra Pradesh</option>
+        <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
+        <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+        <option value="Assam">Assam</option>
+        <option value="Bihar">Bihar</option>
+        <option value="Chandigarh">Chandigarh</option>
+        <option value="Chhattisgarh">Chhattisgarh</option>
+        <option value="Dadar and Nagar Haveli">Dadar and Nagar Haveli</option>
+        <option value="Daman and Diu">Daman and Diu</option>
+        <option value="Delhi">Delhi</option>
+        <option value="Lakshadweep">Lakshadweep</option>
+        <option value="Puducherry">Puducherry</option>
+        <option value="Goa">Goa</option>
+        <option value="Gujarat">Gujarat</option>
+        <option value="Haryana">Haryana</option>
+        <option value="Himachal Pradesh">Himachal Pradesh</option>
+        <option value="Jammu and Kashmir">Jammu and Kashmir</option>
+        <option value="Jharkhand">Jharkhand</option>
+        <option value="Karnataka">Karnataka</option>
+        <option value="Kerala">Kerala</option>
+        <option value="Madhya Pradesh">Madhya Pradesh</option>
+        <option value="Maharashtra">Maharashtra</option>
+        <option value="Manipur">Manipur</option>
+        <option value="Meghalaya">Meghalaya</option>
+        <option value="Mizoram">Mizoram</option>
+        <option value="Nagaland">Nagaland</option>
+        <option value="Odisha">Odisha</option>
+        <option value="Punjab">Punjab</option>
+        <option value="Rajasthan">Rajasthan</option>
+        <option value="Sikkim">Sikkim</option>
+        <option value="Tamil Nadu">Tamil Nadu</option>
+        <option value="Telangana">Telangana</option>
+        <option value="Tripura">Tripura</option>
+        <option value="Uttar Pradesh">Uttar Pradesh</option>
+        <option value="Uttarakhand">Uttarakhand</option>
+        <option value="West Bengal">West Bengal</option>
+      </select>
+    <label for="Pcode"><b>Pincode:</label>
+    <input type = "text" id = "Pcode" name = "Pcode" maxlength = "6" size = "6" pattern="\d{6}">
+    <label for="SPh"><b>Mobile: +91</label>
+    <input type = "text" inputmode="numeric" id = "SPh" name = "SPh" size="10" maxlength = "10" placeholder="xxxxxxxxxxxx">
+    <label for="SEmail"><b>Email:</label>
+    <input type = "email" id = "SEmail" name = "SEmail" size="43" maxlength = "43">
+    <input type = "submit" id = "SAdd" name = "SAdd" value = "Add Record"><br><br>
 
-<form action="forms_action_page.php" method="post">
-<p>Suppliers : Please feed the new supplier name, address and GST. in to the MES System.<p>
-<label for="Sname"><b>Supplier Name: *</label>
-<input type= "text" id = "Sname" name = "Sname" required size="75">
-<label for="SuGST"><b>GSTIN/UIN: *</label>
-<input type = "text" id = "SuGST" name = "SuGST" maxlength = "15" size = "15" required>
-<label for="SIGST"><b>(IGST):</label>
-<input type = "checkbox" id = "SIGST" name = "SIGST">
-<br><br>
-<label for="SAddr"><b>Address:</label>
-<input type = "text" id = "SAddr" name = "SAddr" size="75" maxlength = "75">
-<label for="SCity"><b>City:</label>
-<input type = "text" id = "SCity" name = "SCity" size="32" maxlength = "32">
-<br><br>
-<label for="SState"><b>State:</label>
-<select name="Sstate" id="Sstate">
-<option value="Andhra Pradesh">Andhra Pradesh</option>
-<option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
-<option value="Arunachal Pradesh">Arunachal Pradesh</option>
-<option value="Assam">Assam</option>
-<option value="Bihar">Bihar</option>
-<option value="Chandigarh">Chandigarh</option>
-<option value="Chhattisgarh">Chhattisgarh</option>
-<option value="Dadar and Nagar Haveli">Dadar and Nagar Haveli</option>
-<option value="Daman and Diu">Daman and Diu</option>
-<option value="Delhi">Delhi</option>
-<option value="Lakshadweep">Lakshadweep</option>
-<option value="Puducherry">Puducherry</option>
-<option value="Goa">Goa</option>
-<option value="Gujarat">Gujarat</option>
-<option value="Haryana">Haryana</option>
-<option value="Himachal Pradesh">Himachal Pradesh</option>
-<option value="Jammu and Kashmir">Jammu and Kashmir</option>
-<option value="Jharkhand">Jharkhand</option>
-<option value="Karnataka">Karnataka</option>
-<option value="Kerala">Kerala</option>
-<option value="Madhya Pradesh">Madhya Pradesh</option>
-<option value="Maharashtra">Maharashtra</option>
-<option value="Manipur">Manipur</option>
-<option value="Meghalaya">Meghalaya</option>
-<option value="Mizoram">Mizoram</option>
-<option value="Nagaland">Nagaland</option>
-<option value="Odisha">Odisha</option>
-<option value="Punjab">Punjab</option>
-<option value="Rajasthan">Rajasthan</option>
-<option value="Sikkim">Sikkim</option>
-<option value="Tamil Nadu">Tamil Nadu</option>
-<option value="Telangana">Telangana</option>
-<option value="Tripura">Tripura</option>
-<option value="Uttar Pradesh">Uttar Pradesh</option>
-<option value="Uttarakhand">Uttarakhand</option>
-<option value="West Bengal">West Bengal</option>
-</select>
-<label for="Pcode"><b>Pincode:</label>
-<input type = "text" id = "Pcode" name = "Pcode" maxlength = "6" size = "6" pattern="\d{6}">
-<label for="SPh"><b>Mobile: +91</label>
-<input type = "text" inputmode="numeric" id = "SPh" name = "SPh" size="10" maxlength = "10" placeholder="xxxxxxxxxxxx">
-<label for="SEmail"><b>Email:</label>
-<input type = "email" id = "SEmail" name = "SEmail" size="43" maxlength = "43">
-<input type = "submit" id = "SAdd" name = "SAdd" value = "Add Record">
-<br><br>
+    <table id="myTable">
+      <tr>
+          <th>Supplier ID</th>
+          <th>Supplier Name</th>
+          <th>GSTIN/UIN</th>
+          <th>Address</th>
+          <th>City</th>
+          <th>State</th>
+          <th>Pin Code</th>
+          <th>Phone</th>
+          <th>Email</th>
+          <th>CompanyID</th>
+          <th>IGST</th>
+      </tr>
+            <?php
+            // Loop through the array to generate table rows
+            foreach ($dbtabdata as $row) {
+                echo "<tr>";
+                foreach ($row as $cell) {
+                    echo "<td>$cell</td>";
+                  }      
+                echo "</tr>";
+            }
+            ?>
+    </table>
+    </div>
+  </form>
+</div>
+<div class="form-popup" id="myForm">
+  <form action="forms_action_page.php" class="form-container" method="post">
+  <input type = "text" id = "SID" name = "SID" maxlength = "6" size = "6" disabled hidden>
+   <label for="Sname2"><b>Supplier Name: * &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+   <button style="font-size:18px" input type = "submit" ><i class="fa fa-save" ></i></button>
+   <button style="font-size:18px" input type = "submit" ><i class="fa fa-trash-o"></i></button>
+   <input type = "button" style="font-size:18px" class = "cancel" id = "Scancel" name = "Scancel" value = "X" onclick= "closeForm()">
+   <input type= "text" id = "Sname2" name = "Sname2" required size="75" disabled>
+   <label for="SuGST2"><b>GSTIN/UIN: *</label>
+   <input type = "text" id = "SuGST2" name = "SuGST2" maxlength = "15" size = "15" required>
+   <label for="SIGST2"><b>(IGST):</label>
+   <input type = "checkbox" id = "SIGST2" name = "SIGST2"><br><br>
+   <label for="SAddr2"><b>Address:</label>
+   <input type = "text" id = "SAddr2" name = "SAddr2" size="75" maxlength = "75">
+   <label for="SCity2"><b>City:</label>
+   <input type = "text" id = "SCity2" name = "SCity2" size="32" maxlength = "32">
+   <label for="SState2"><b>State:</label>
+     <select name="Sstate2" id="Sstate2" input type="select">
+       <option value="Andhra Pradesh">Andhra Pradesh</option>
+       <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
+       <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+       <option value="Assam">Assam</option>
+       <option value="Bihar">Bihar</option>
+       <option value="Chandigarh">Chandigarh</option>
+       <option value="Chhattisgarh">Chhattisgarh</option>
+       <option value="Dadar and Nagar Haveli">Dadar and Nagar Haveli</option>
+       <option value="Daman and Diu">Daman and Diu</option>
+       <option value="Delhi">Delhi</option>
+       <option value="Lakshadweep">Lakshadweep</option>
+       <option value="Puducherry">Puducherry</option>
+       <option value="Goa">Goa</option>
+       <option value="Gujarat">Gujarat</option>
+       <option value="Haryana">Haryana</option>
+       <option value="Himachal Pradesh">Himachal Pradesh</option>
+       <option value="Jammu and Kashmir">Jammu and Kashmir</option>
+       <option value="Jharkhand">Jharkhand</option>
+       <option value="Karnataka">Karnataka</option>
+       <option value="Kerala">Kerala</option>
+       <option value="Madhya Pradesh">Madhya Pradesh</option>
+       <option value="Maharashtra">Maharashtra</option>
+       <option value="Manipur">Manipur</option>
+       <option value="Meghalaya">Meghalaya</option>
+       <option value="Mizoram">Mizoram</option>
+       <option value="Nagaland">Nagaland</option>
+       <option value="Odisha">Odisha</option>
+       <option value="Punjab">Punjab</option>
+       <option value="Rajasthan">Rajasthan</option>
+       <option value="Sikkim">Sikkim</option>
+       <option value="Tamil Nadu">Tamil Nadu</option>
+       <option value="Telangana">Telangana</option>
+       <option value="Tripura">Tripura</option>
+       <option value="Uttar Pradesh">Uttar Pradesh</option>
+       <option value="Uttarakhand">Uttarakhand</option>
+       <option value="West Bengal">West Bengal</option>
+	   </select>
+   <label for="SPcode2"><b>Pincode:</label>
+   <input type = "text" id = "SPcode2" name = "SPcode2" maxlength = "6" size = "6" pattern="\d{6}">
+   <label for="SPh2"><b>Mobile: +91</label>
+   <input type = "text" inputmode="numeric" id = "SPh2" name = "SPh2" size="10" maxlength = "10" placeholder="xxxxxxxxxxxx">
+   <label for="SEmail2"><b>Email:</label>
+   <input type = "email" id = "SEmail2" name = "SEmail2" size="43" maxlength = "43">
 
-<table>
-  <tr>
-    <th>Supplier ID</th>
-    <th>Supplier Name</th>
-    <th>GSTIN/UIN</th>
-    <th>Address</th>
-    <th>City</th>
-    <th>State</th>
-    <th>Pin Code</th>
-    <th>Phone</th>
-    <th>Email</th>
-    <th>CompanyID</th>
-    <th>IGST</th>
-  </tr>
-        <?php
-        // Loop through the array to generate table rows
-        foreach ($dbtabdata as $row) {
-            echo "<tr>";
-            $i = 0;
-            foreach ($row as $cell) {
-              if ($i == 0) {
-              echo "<td><a href='editsupplier.php'>$cell</a></td>";
-              } else {
-                echo "<td>$cell</td>";
-              }
-              $i++;
-                }         
-            echo "</tr>";
-        }
-        ?>
-</table>
+  </form>
+</div>
 
+<script>
+  document.getElementById("myForm").style.display = "none";
+  /*
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  */
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  // Get the table element
+  var table = document.getElementById("myTable");
+
+  // Attach a click event listener to the table
+  table.addEventListener("click", function(event) {
+    // Check if the clicked element is a table row
+    if (event.target.tagName === "TD") {
+      document.getElementById("myForm").style.display = "block";
+      // Get the data of the clicked row
+      var row = event.target.parentNode; // Get the parent row (<tr>)
+      var cells = row.getElementsByTagName("td"); // Get all cells (<td>) in the row
+      // Extract the data from cells
+      var id_2 = cells[0].innerText;
+      var name = cells[1].innerText;
+      var gstin = cells[2].innerText;
+      var addr = cells[3].innerText;
+      var city = cells[4].innerText;
+      var state = cells[5].innerText;
+      var pincode_1 = cells[6].innerText;
+      var phone = cells[7].innerText;
+      var email = cells[8].innerText;
+      var igst = cells[10].innerText;
+
+      // Set the values of the form fields
+      document.getElementById("SID").value = id;
+      document.getElementById("Sname2").value = name;
+      document.getElementById("SuGST2").value = gstin;
+      document.getElementById("SAddr2").value = addr;
+      document.getElementById("SCity2").value = city;
+      document.getElementById("SState2").value = state;
+      document.getElementById("SPcode2").value = pincode_1;
+      document.getElementById("SPh2").value = phone;
+      document.getElementById("SEmail2").value = email;
+      document.getElementById("SIGST2").value = igst;
+      
+    }
+  });
+</script>
+
+</script>
 </body>
 </html> 
