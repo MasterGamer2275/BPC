2a99e1601351ec9e85191b539b6d0e124df802e9
MasterGamer2275 <MasterGamer2275@users.noreply.github.com>
2024-04-20 19:57:05 +0530

    ui updates for release

--- src/main-page/runningquotes.php
+++ src/main-page/runningquotes.php
@@ -18,7 +18,7 @@ img {vertical-align: middle;}
   background-color: rgb(173, 103, 79);
   color: yellow;
   display:active;
-  font-size: 13px;
+  font-size: 17px;
 }
 
 .quotes-textarea {
@@ -29,7 +29,7 @@ img {vertical-align: middle;}
   color: yellow;
   border: none;
   //font-size: 130%;
-  font-size: 12px;
+  font-size: 1.35em;
 }
 
 .active {
@@ -39,7 +39,7 @@ img {vertical-align: middle;}
 /* Fading animation */
 .fade {
   animation-name: fade;
-  animation-duration: 1.5s;
+  animation-duration: 1.2s;
 }
 
 @keyframes fade {
@@ -47,22 +47,37 @@ img {vertical-align: middle;}
   to {opacity: 1}
 }
 
-/* On smaller screens, decrease text size */
-@media only screen and (max-width: 300px) {
-  .text {font-size: 11px}
-}
+  @media screen and (max-width: 600px) {
+    /* Adjust font size for smaller screens */
+    quotes-textarea {
+      font-size: 0.5em;
+    }
+  }
+
+  @media screen and (min-width: 1200px) {
+    /* Adjust font size for larger screens */
+    quotes-textarea {
+      font-size: 1.2em;
+    }
+  }
+  @media screen and (max-width: 300px) {
+    /* Adjust font size for larger screens */
+    quotes-textarea {
+      display: none;
+    }
+  }
 </style>
 </head>
 <body>
 
 <div class="quotes-textarea">
-
+  <p class="quotelinks">"Imagination is more important than knowledge. For knowledge is limited to all we know and understand, while imagination embraces the entire world, and all there ever will be to know and understand." – Albert Einstein</p>
   <p class="quotelinks">“All that we are is the result of what we have thought. The mind is everything. What we think we become.” – Gautama Buddha.</p>
   <p class="quotelinks">“Political tyranny is nothing compared to social tyranny and a reformer who defies society is a more courageous man than a politician who defies government.” – B. R. Ambedkar</p>
   <p class="quotelinks">“The tree laden with fruits always bends low. If you wish to be great, be lowly and meek.” – Sri Ramakrishna Paramahamsa</p>
   <p class="quotelinks">“Democracy is good. I say this because other systems are worse.” – Jawaharlal Nehru</p>
   <p class="quotelinks">“We are what our thoughts have made us; so take care about what you think. Words are secondary. Thoughts live; they travel far.” – Swami Vivekananda</p>
-  <p class="quotelinks">“No one would starve in independent India. Its grain would not be exported. Cloth would not be imported by it. Its leaders would not use a foreign language and finding justice in it would be neither costly nor difficult.” – Sardar Patel</p>
+  <p class="quotelinks">“If you can't explain it simply, you don't understand it well enough.” – Albert Einstein</p>
   <p class="quotelinks">“Prophet Mohammed would have no objection to The Satanic Verses.” – Salman Rushdie</p>
   <p class="quotelinks">“Arise, Awake and Stop not until the goal is reached.” – Swami Vivekananda.</p>
   <p class="quotelinks">“I am not very good at statistics. I am also a poor thinker.” – Manmohan Singh"</p>
@@ -71,7 +86,7 @@ img {vertical-align: middle;}
   <p class="quotelinks">“You must learn to be still in the midst of activity and to be vibrantly alive in repose.” – Indira Gandhi</p>
   <p class="quotelinks">“A politician should not be written off till he or she is cremated.” – George Fernandes</p>
   <p class="quotelinks">“There is an important distinction between barriers to entry and barriers to imitation.” – C.K.Prahalad</p>
-  <p class="quotelinks">“I was assured of a place in the team without trials, but I had a feeling it was not fair…when many of my friends were fighting badly for a place and had to prove their mettle in the inter-provincial tournament.” – Dhyan</p>
+  <p class="quotelinks">“We cannot solve our problems with the same thinking we used when we created them.” – Albert Einstein</p>
   <p class="quotelinks">“To succeed in your mission, you must have single-minded devotion to your goal.” – Abdul Kalam</p>
   <p class="quotelinks">“No apology is necessary for being truthful to the echoes of one’s mother tongue.” – Mulk Raj Anand</p>
   <p class="quotelinks">“Do not say, ‘It is morning,’ and dismiss it with a name of yesterday. See it for the first time as a newborn child that has no name.” – Rabindranath Tagore</p>
@@ -80,8 +95,7 @@ img {vertical-align: middle;}
   <p class="quotelinks">“After all, our Killers are our …Brothers!!??” – Bal Gangadhara Tilak</p>
   <p class="quotelinks">“No girl has offered to go to bed with me in return for a part (in a film). I wish it would happen.” – Manmohan Desai</p>
   <p class="quotelinks">“Education is the manifestation of perfection already in a man.” – Swami Vivekananda</p>
-  <p class="quotelinks">“Indian men are still used to the traditional role given to women; they want intelligent girls for company but not to marry…(they) have yet to taste and relish the company of mature women.” – Amrita Pritam</p>
-  <p class="quotelinks">"Imagination is more important than knowledge. For knowledge is limited to all we know and understand, while imagination embraces the entire world, and all there ever will be to know and understand." – Albert Einstein</p>
+  <p class="quotelinks">“We cannot solve our problems with the same thinking we used when we created them."– Albert Einstein</p>
   <p class="quotelinks">“You must be the change you want to see in the world.” – Mahatma Gandhi.</p>
 </div>
 
